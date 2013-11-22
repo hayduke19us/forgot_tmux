1f47c43a59aeb7d90286c80362e5be78f7205f28
hayduke19us <hayduke19us@gmail.com>
2013-11-22 14:00:58 -0600

    control structure built for Simple walk thru

--- simple.rb
+++ simple.rb
@@ -12,42 +12,49 @@ module Simple
     puts "would you like to create key bindings?" 
     input = gets.chomp
     if input =~ (/\Ay|yes\z/)
-      Simple.key_binding_walk
+     key =  Simple::Key.new
+     key.choices
     end
   end
-  def self.question_helper
-    input = gets.chomp
-    input
-  end
   
-  def self.key_binding_walk
+  class Key
+    def self.yes_or 
+      input = gets.chomp
+      input
+    end
     
-     puts "change PREFIX to  ctrl a? (y|n)"
-     q = Simple.question_helper 
-     if  q  =~ /y/
-       Bindings.prefix
-     end
+    def choices   
+      puts "change PREFIX to  ctrl a? (y|n)"
+      if Key.yes_or =~ /y/  
+        Bindings.prefix
+      end
+       
+      puts "change delay of commands to interact with vim better? (y|n)"
+      if Key.yes_or =~ /y/
+        Bindings.escape_time  
+      end
+
+      puts "set a source ~/tmux.conf shortcut? (y|n)"
+      if Key.yes_or =~ /y/   
+        Bindings.reload_conf
+      end
      
-     puts "change delay of commands to interact with vim better? (y|n)"
-     q
-     if q =~ /y/
-     Bindings.escape_time  
-     end
-     puts "set a source ~/tmux.conf shortcut? (y|n)"
-     Bindings.reload_conf
-     
-     puts "send PREFIX to other apps like vim with pressing PREFX * 2"
-     Bindings.send_prefix
-
-     puts "split window into horizontal pane with PREFIX + |"
-     Bindings.horizontal_pane
-
-     puts "split window into vertical pane with PREFIX + -"
-     Bindings.vertical_pane
-
-      
-   Setup.divider
-      
+      puts "send PREFIX to other apps like vim with pressing PREFX * 2"
+      if Key.yes_or =~ /y/
+        Bindings.send_prefix
+      end
+    
+      puts "split window into horizontal pane with PREFIX + |"
+      if Key.yes_or =~ /y/
+        Bindings.horizontal_pane
+      end
+         
+      puts "split window into vertical pane with PREFIX + -"
+      if Key.yes_or =~ /y/
+        Bindings.vertical_pane
+      end
+    Setup.divider
+   end  
   end
 
 end
